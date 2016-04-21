ace227232b6fc02d9f6e8afda31aa494887ef0ab
Roger Gomez Olivares <rogerscuall@users.noreply.github.com>
2016-04-20 22:13:36 -0400

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
-
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
